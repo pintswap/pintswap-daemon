085e5f0281afc1cf7a76b84397a32c9337b5f820
pintswap <inquiries@pintswap.exchange>
2023-08-04 20:22:46 +0000

    update

--- bin/daemon.js
+++ bin/daemon.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 
+const yargs = require('yargs');
+const { ethers } = require('ethers');
+yargs.parserConfiguration({ 'parse-numbers': false });
+const options = { ...yargs.argv };
+const subcommand = options._;
+delete options._;
+const rpcHost = options['rpc-host'];
+const rpcPort = options['rpc-port'];
+const wallet = options.wallet;
+if (wallet) {
+  if (ethers.Mnemonic.isValidMnemonic(wallet)) process.env.PINTSWAP_DAEMON_WALLET = new ethers.Wallet.fromPhrase(wallet).privateKey;
+  else process.env.PINTSWAP_DAEMON_WALLET = wallet;
+}
+if (rpcHost) process.env.PINTSWAP_DAEMON_HOST = rpcHost;
+if (rpcPort) process.env.PINTSWAP_DAEMON_PORT = rpcPort;
 const { run, logger } = require('../');
 
 (async () => {
