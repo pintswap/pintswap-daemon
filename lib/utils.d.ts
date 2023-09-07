d16a1667e6e06af1fbb44a6fe71fd05fb825f99a
pintswap <pintswap@pintswap.exchange>
2023-09-07 22:23:07 +0000

    update

---
+++ lib/utils.d.ts
@@ -0,0 +1,3 @@
+export declare function waitForBlock(provider: any, number: any): Promise<any>;
+export declare const timeout: (n: any) => Promise<unknown>;
+export declare function sendBundle(logger: any, flashbots: any, txs: any, blockNumber: any): any;
